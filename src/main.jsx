6939cb1b1b7ccff3efa80d7d7cb31188e8627fb5
falck5561-ux <falck5561@gmail.com>
2025-10-15 01:43:38 -0600

    Fix(CORS): Habilita withCredentials para solucionar bloqueo de peticiones GET en producción

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { AuthProvider } from './context/AuthContext.jsx';
-import { ThemeProvider } from './context/ThemeContext.jsx';
-import { CartProvider } from './context/CartContext.jsx'; // 1. IMPORTAR EL CARTPROVIDER
+import { ThemeProvider } from './context/ThemeProvider.jsx';
+import { CartProvider } from './context/CartContext.jsx'; 
 import axios from 'axios';
 
 // Establecemos la URL base para todas las peticiones
 axios.defaults.baseURL = 'https://tito-cafe-backend.onrender.com';
+
+// **LÍNEA AÑADIDA PARA SOLUCIONAR CORS/AUTH EN PETICIONES GET**
+// Esto asegura que Axios incluya las credenciales (cookies/autorización)
+// en las peticiones entre dominios, lo cual es vital en Render.
+axios.defaults.withCredentials = true;
 
 // Configuramos Axios para que envíe el token en cada petición
 axios.interceptors.request.use(config => {
@@ -27,7 +32,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
       <ThemeProvider>
-        <CartProvider> {/* 2. ENVOLVER LA APP CON EL CARRITO */}
+        <CartProvider>
           <App />
         </CartProvider>
       </ThemeProvider>
