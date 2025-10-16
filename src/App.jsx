490687a19e2fcce815b923f3613cb083cfc95c49
falck5561-ux <falck5561@gmail.com>
2025-10-16 12:50:41 -0600

    feat(home): hhh

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-// Archivo: src/App.jsx (Versión Final, Completa y Refactorizada)
+// Archivo: src/App.jsx
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 
 // Importación de Componentes
-import Navbar from './components/Navbar'; // <-- Importamos el Navbar externo
+import Navbar from './components/Navbar';
 import ProtectedRoute from './components/ProtectedRoute';
 
 // Importación de las Páginas
@@ -15,27 +15,31 @@ import AdminPage from './pages/AdminPage';
 import PosPage from './pages/PosPage';
 import ClientePage from './pages/ClientePage';
 import CanjearPage from './pages/CanjearPage';
-import CombosPage from './pages/CombosPage'; // <-- 1. IMPORTA LA NUEVA PÁGINA DE COMBOS
+import CombosPage from './pages/CombosPage';
 
 function App() {
   return (
     <BrowserRouter>
-      <Navbar /> {/* <-- Usamos el componente Navbar */}
+      <Navbar />
       <Toaster position="top-center" reverseOrder={false} />
       
-      {/* El contenedor principal se mueve aquí para aplicar a todas las páginas */}
       <div className="container mt-4">
         <Routes>
           {/* --- Rutas Públicas --- */}
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
-          <Route path="/combos" element={<CombosPage />} /> {/* <-- 2. AÑADE LA NUEVA RUTA */}
+          <Route path="/combos" element={<CombosPage />} />
 
           {/* --- Rutas Protegidas --- */}
           <Route path="/admin" element={<ProtectedRoute roles={['Jefe']}><AdminPage /></ProtectedRoute>} />
           <Route path="/pos" element={<ProtectedRoute roles={['Empleado', 'Jefe']}><PosPage /></ProtectedRoute>} />
           <Route path="/cliente" element={<ProtectedRoute roles={['Cliente']}><ClientePage /></ProtectedRoute>} />
+          
+          {/* <-- ¡AQUÍ ESTÁ LA NUEVA RUTA AÑADIDA! --> */}
+          {/* Esto le dice a la app que /hacer-un-pedido también debe mostrar la página del cliente. */}
+          <Route path="/hacer-un-pedido" element={<ProtectedRoute roles={['Cliente']}><ClientePage /></ProtectedRoute>} />
+
           <Route path="/canjear" element={<ProtectedRoute roles={['Empleado', 'Jefe']}><CanjearPage /></ProtectedRoute>} />
           
         </Routes>
