d989ed8bd8530a174352a0ea65f73bee1d27784f
falck5561-ux <falck5561@gmail.com>
2025-10-19 00:35:35 -0600

    feat(pwa): Implementar PWA para que la app sea instalable

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Tito Café',
+        short_name: 'TitoCafé',
+        description: 'La aplicación oficial de Tito Café.',
+        theme_color: '#ffffff', // Puedes cambiar este color
+        icons: [
+          {
+            src: 'tito-icon.png', // <-- Tu archivo de la carpeta public
+            sizes: '192x192',     // <-- Revisa que este sea el tamaño real
+            type: 'image/png'
+          },
+          {
+            src: 'logo-inicio.png', // <-- Tu otro archivo de la carpeta public
+            sizes: '512x512',       // <-- Revisa que este sea el tamaño real
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
 })
